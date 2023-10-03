34a00c9c5544236fef06f1a50f97ef64085eccd2
ibito23raysuken <ibitoraysuken@gmail.com>
2023-10-03 19:26:30 +0300

    second commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,13 +12,8 @@ export default function Footer() {
       bg={useColorModeValue("gray.50", "gray.900")}
       color={useColorModeValue("gray.700", "gray.200")}
     >
-      <Container
-        as={Stack}
-        maxW={"6xl"}
-        py={4}
-        align="center"
-      >
-        <Text>© 2023 Eldora Boo. All rights reserved</Text>
+      <Container as={Stack} maxW={"6xl"} py={4} align="center">
+        <Text>© 2023 A. Faniry N. All rights reserved</Text>
       </Container>
     </Box>
   );
